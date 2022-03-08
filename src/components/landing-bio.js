3c1fd8ebffca6cd3cf9e18874f5b5053e921333c
Carlos Trujillo <krloslao90@gmail.com>
2022-03-08 00:14:11 -0500

    updating episode

--- src/components/landing-bio.js
+++ src/components/landing-bio.js
@@ -1,16 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-import { Button, Form } from 'react-bootstrap'
+import { Button, Form } from "react-bootstrap"
 
 import styled from "@emotion/styled"
+import applepodcast_logo from "../images/applepodcast@2x.png"
 import spotify_logo from "../images/spotify@2x.png"
 import google_logo from "../images/google_podcasts@2x.png"
 import pocketcasts_logo from "../images/pocket_casts@2x.png"
 import overcast_logo from "../images/overcast@2x.png"
 import radiopublic_logo from "../images/radiopublic@2x.png"
 import breaker_logo from "../images/breaker@2x.png"
-
 
 const Container = styled.div`
   text-align: center;
@@ -40,34 +40,181 @@ const LandingBio = () => (
           }
         }
       }
-    `}    
-    render={data => (
+    `}
+    render={(data) => (
       <OuterContainer>
         <Container>
-          <span class="avatar"><img src="images/avatar.jpg" alt="" /></span>
-          <h1 style={{letterSpacing: '1px', fontSize: '50px', fontFamily: 'Trebuchet MS'}}>Entre Cubanos</h1>
-          <p style={{letterSpacing: 'normal'}}>Un podcast de <a href="https://www.facebook.com/carlostrujillo1990/" target="_blank" rel="noreferrer" style={{textDecoration: 'underline'}}>Carlos Trujillo</a></p>
-          <p>Ultimo Episodio</p>
-          <iframe title="Podcast Player" src="https://anchor.fm/entrecubanos/embed/episodes/03--Patria-Y-Libertad-erkiu5" height="102px" width="400px" frameborder="0" scrolling="no" style={{width: '100%', marginBottom: '16px'}}></iframe>
-          <p>Escucha mas episodios en <br />
-            <a href="https://open.spotify.com/show/7tPegKMaz5ePNDyqEL9z84" target='_blank' rel="noreferrer"><img src={spotify_logo} className="podcast-img" alt="Spotify" style={{ width: '40px',	height: '40px', margin: '12px 4px 0'}}/></a>
-            <a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy8xMmNlNzMzOC9wb2RjYXN0L3Jzcw==" target='_blank' rel="noreferrer"><img src={google_logo} className="podcast-img" alt="Google Podcast" style={{border: '1px solid lightsalmon', borderRadius: '6px', width: '40px',	height: '40px', margin: '12px 4px 0'}} /></a>
-            <a href="https://pca.st/b9ue3b66" target='_blank' rel="noreferrer"><img src={pocketcasts_logo} className="podcast-img" alt="Pocketcast" style={{ width: '40px',	height: '40px', margin: '12px 4px 0'}}/></a>
-            <a href="https://overcast.fm/itunes1501367579/entre-cubanos" target='_blank' rel="noreferrer"><img src={overcast_logo} className="podcast-img" alt="Overcast" style={{ width: '40px',	height: '40px', margin: '12px 4px 0'}}/></a>
-            <a href="https://radiopublic.com/entre-cubanos-8g1V2p" target='_blank' rel="noreferrer"><img src={radiopublic_logo} className="podcast-img" alt="Radio Public" style={{ width: '40px',	height: '40px', margin: '12px 4px 0'}}/></a>								
-            <a href="https://www.breaker.audio/entre-cubanos" target='_blank' rel="noreferrer"><img src={breaker_logo} className="podcast-img" alt="Breaker" style={{ width: '40px',	height: '40px', margin: '12px 4px 0'}}/></a>								
-          </p>
-          <h5 style={{marginBottom: '4px'}}>Suscríbete para recibir actualizaciones</h5>
+          <span class="avatar">
+            <img src="images/avatar.jpg" alt="" />
+          </span>
+          <h1
+            style={{
+              letterSpacing: "1px",
+              fontSize: "50px",
+              fontFamily: "Trebuchet MS",
+            }}
+          >
+            Entre Cubanos
+          </h1>
 
-          <Form action="https://gmail.us1.list-manage.com/subscribe/post?u=faf1f03b14863edbee03848e0&amp;id=502c238860" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" novalidate>
-            <Form.Group controlId="formBasicName">              
-              <Form.Control type="text" name="FNAME" placeholder="Nombre" style={{width: '60%', margin: '8px 0', paddingLeft: '10px', border: '1px solid black', borderRadius: '8px'}}/>
+          <p>Ultimo Episodio</p>
+          <iframe
+            title="Podcast Player"
+            src="https://anchor.fm/entrecubanos/embed/episodes/05--Energia--Crisis-en-Texas---ser-Ingeniero-en-EEUU-e1fd5cl"
+            height="102px"
+            width="400px"
+            frameborder="0"
+            scrolling="no"
+            style={{ width: "100%", marginBottom: "16px" }}
+          ></iframe>
+          <p>
+            Escucha mas episodios en <br />
+            <a
+              href="https://open.spotify.com/show/7tPegKMaz5ePNDyqEL9z84"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={spotify_logo}
+                className="podcast-img"
+                alt="Spotify"
+                style={{ width: "40px", height: "40px", margin: "12px 4px 0" }}
+              />
+            </a>
+            <a
+              href="https://podcasts.apple.com/us/podcast/entre-cubanos/id1501367579"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={applepodcast_logo}
+                className="podcast-img"
+                alt="Apple Podcast"
+                style={{ width: "40px", height: "40px", margin: "12px 4px 0" }}
+              />
+            </a>
+            <a
+              href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy8xMmNlNzMzOC9wb2RjYXN0L3Jzcw=="
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={google_logo}
+                className="podcast-img"
+                alt="Google Podcast"
+                style={{
+                  border: "1px solid lightsalmon",
+                  borderRadius: "6px",
+                  width: "40px",
+                  height: "40px",
+                  margin: "12px 4px 0",
+                }}
+              />
+            </a>
+            <a href="https://pca.st/b9ue3b66" target="_blank" rel="noreferrer">
+              <img
+                src={pocketcasts_logo}
+                className="podcast-img"
+                alt="Pocketcast"
+                style={{ width: "40px", height: "40px", margin: "12px 4px 0" }}
+              />
+            </a>
+            <a
+              href="https://overcast.fm/itunes1501367579/entre-cubanos"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={overcast_logo}
+                className="podcast-img"
+                alt="Overcast"
+                style={{ width: "40px", height: "40px", margin: "12px 4px 0" }}
+              />
+            </a>
+            <a
+              href="https://radiopublic.com/entre-cubanos-8g1V2p"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={radiopublic_logo}
+                className="podcast-img"
+                alt="Radio Public"
+                style={{ width: "40px", height: "40px", margin: "12px 4px 0" }}
+              />
+            </a>
+            <a
+              href="https://www.breaker.audio/entre-cubanos"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={breaker_logo}
+                className="podcast-img"
+                alt="Breaker"
+                style={{ width: "40px", height: "40px", margin: "12px 4px 0" }}
+              />
+            </a>
+          </p>
+          <h5 style={{ marginBottom: "4px" }}>
+            Suscríbete para recibir actualizaciones
+          </h5>
+
+          <Form
+            action="https://gmail.us1.list-manage.com/subscribe/post?u=faf1f03b14863edbee03848e0&amp;id=502c238860"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            class="validate"
+            novalidate
+          >
+            <Form.Group controlId="formBasicName">
+              <Form.Control
+                type="text"
+                name="FNAME"
+                placeholder="Nombre"
+                style={{
+                  width: "60%",
+                  margin: "8px 0",
+                  paddingLeft: "10px",
+                  border: "1px solid black",
+                  borderRadius: "8px",
+                }}
+              />
             </Form.Group>
             <Form.Group controlId="formBasicEmail">
-              <Form.Control name="EMAIL" type="email" placeholder="Email" style={{width: '60%', margin: '8px 0 16px', paddingLeft: '10px', border: '1px solid black', borderRadius: '8px'}}/>
-            </Form.Group>            
-            <Button variant="primary" type="submit" name="subscribe" style={{"backgroundColor":"#44c767","borderRadius":"28px","border":"1px solid #18ab29","display":"inline-block","cursor":"pointer","color":"#ffffff","fontFamily":"Arial","fontSize":"17px","padding":"8px 28px","textDecoration":"none","textShadow":"0px 1px 0px #2f6627"}}>
-              Suscribir 
+              <Form.Control
+                name="EMAIL"
+                type="email"
+                placeholder="Email"
+                style={{
+                  width: "60%",
+                  margin: "8px 0 16px",
+                  paddingLeft: "10px",
+                  border: "1px solid black",
+                  borderRadius: "8px",
+                }}
+              />
+            </Form.Group>
+            <Button
+              variant="primary"
+              type="submit"
+              name="subscribe"
+              style={{
+                backgroundColor: "#44c767",
+                borderRadius: "28px",
+                border: "1px solid #18ab29",
+                display: "inline-block",
+                cursor: "pointer",
+                color: "#ffffff",
+                fontFamily: "Arial",
+                fontSize: "17px",
+                padding: "8px 28px",
+                textDecoration: "none",
+                textShadow: "0px 1px 0px #2f6627",
+              }}
+            >
+              Suscribir
             </Button>
           </Form>
         </Container>
